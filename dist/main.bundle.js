e39f4d7f7c81bca2d3d11d5ce316be9fb598c4a7
Jawad Malik <jmalik94@outlook.com>
2024-04-05 11:23:59 +0100

    project initalised

---
+++ dist/main.bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('testing');
+/******/ })()
+;
+//# sourceMappingURL=main.bundle.js.map
